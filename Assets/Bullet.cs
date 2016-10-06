1b1385a404bc99e1b218e99ce7f55c7f1b403878
Kevin Smith <thirdwiggin@gmail.com>
2016-10-05 21:24:44 -0600

    Fires bullets

---
+++ Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 1000f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public void setDir (float dir)
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        float x;
+        if (dir < 0)
+            x = -speed;
+        else
+            x = speed;
+
+        rb.velocity = new Vector2(x, 0);
+        Debug.Log(rb.velocity);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
--- Assets/Character.cs
+++ Assets/Character.cs
@@ -3,31 +3,47 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
-    public Transform point1;
-    public Transform point2;
-    public float castDistance = 0.1f;
     public float moveSpeed = 20f;
+    public float jumpForce = 300f;
+    public float fireRate = 5f;
+    public Bullet bullet;
 
     private int surfaceLayerIdx;
     private bool onGround;
+    private Sprite sprite;
+    private float lastFire = 0;
+    private float dirFacing = 1f;
 
-    bool isOnGround()
-    {
-        RaycastHit2D hit1 = Physics2D.Raycast(point1.position, Vector2.down, castDistance);
-        RaycastHit2D hit2 = Physics2D.Raycast(point1.position, Vector2.down, castDistance);
-
-        return hit1 || hit2;
+    void faceRight() {
+        dirFacing = 1f;
+        transform.localScale = new Vector3(-1, 1, 1);
     }
 
-	// Use this for initialization
+    void faceLeft() {
+        dirFacing = -1f;
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+
 	void Start () {
         surfaceLayerIdx = LayerMask.NameToLayer("Surface");
+        sprite = GetComponent<Sprite>();
 	}
 
-	void OnTriggerEnter2D (Collider2D c)
-    {
-        if (c.gameObject.layer == surfaceLayerIdx)
-        {
+    void spawnBullet () {
+        Transform muzzle = transform.Find("Muzzle");
+        Bullet b = (Bullet)Instantiate(bullet, muzzle.position, Quaternion.identity);
+        b.setDir(dirFacing);
+    }
+
+    void fireAction () {
+        float now = Time.time;
+        if (now - lastFire >= 1 / fireRate) {
+            spawnBullet();
+            lastFire = now;
+        }
+    }
+	void OnTriggerEnter2D (Collider2D c) {
+        if (c.gameObject.layer == surfaceLayerIdx) {
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
             Debug.Log("OnGround");
             onGround = true;
@@ -35,36 +51,37 @@ public class Character : MonoBehaviour {
         }
     }
 
-	void OnTriggerExit2D (Collider2D c)
-    {
-        if (c.gameObject.layer == surfaceLayerIdx)
-        {
+	void OnTriggerExit2D (Collider2D c) {
+        if (c.gameObject.layer == surfaceLayerIdx) {
             Debug.Log("InAir");
             onGround = false;
         }
     }
 
-    void FixedUpdate ()
-    {
+    void FixedUpdate () {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         float motion = Input.GetAxis("Horizontal");
         float move = motion * moveSpeed * Time.fixedDeltaTime;
         rb.velocity = new Vector2(move, rb.velocity.y);
+        if (motion < 0)
+            faceLeft();
+        else if (motion > 0)
+            faceRight();
     }
+
 	// Update is called once per frame
 	void Update () {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (onGround)
-            {
+        if (Input.GetButtonDown("Jump")) {
+            if (onGround) {
                 Debug.Log("Jump");
-                rb.AddForce(Vector2.up * 300);
-            }
-            else
-            {
+                rb.AddForce(Vector2.up * jumpForce);
+            } else {
                 Debug.Log("Not on ground");
             }
+        }
+        if (Input.GetButton("Fire1")) {
+            fireAction();
         }
 	}
 }
